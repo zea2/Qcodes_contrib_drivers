d4c121c3fcea9410d2dad43c66e6222b0947b9e0
Lukas Lankes <l.lankes@fz-juelich.de>
2020-10-15 12:29:23 +0200

    More commands and bug fixes in Sirah Matisse driver

--- qcodes_contrib_drivers/drivers/Sirah/Matisse.py
+++ qcodes_contrib_drivers/drivers/Sirah/Matisse.py
@@ -21,12 +21,13 @@ class SirahMatisseMotor(InstrumentChannel, abc.ABC):
     def __init__(self, parent: "SirahMatisse", name: str, cmd_prefix: str, **kwargs):
         super().__init__(parent, name, **kwargs)
 
-        self._cmd_prefix = ":" + cmd_prefix.strip() + ":"
+        self._cmd_prefix = cmd_prefix.strip() + ":"
 
         self.add_parameter("position",
                            label="Current position of motor",
                            get_cmd=self._cmd_prefix + "POS?",
                            set_cmd=self._cmd_prefix + "POS {}",
+                           get_parser=self._int_parser,
                            vals=vals.Ints(),
                            # TODO unit="???", # steps?
                            docstring="""
@@ -63,6 +64,7 @@ class SirahMatisseMotor(InstrumentChannel, abc.ABC):
                            label="Maximum position of stepper "
                                  "motor.",
                            get_cmd=self._cmd_prefix + "MAX?",
+                           get_parser=self._int_parser,
                            vals=vals.Ints(),
                            # TODO unit="???",
                            docstring="""Gets the maximum position of the stepper motor.""")
@@ -101,12 +103,21 @@ class SirahMatisseMotor(InstrumentChannel, abc.ABC):
                            label="Number of steps made by birefringent filter motor",
                            get_cmd=self._cmd_prefix + "INC?",
                            set_cmd=self._cmd_prefix + "INC {}",
+                           get_parser=self._int_parser,
                            vals=vals.Ints(),
                            # TODO unit="???", # steps?
                            docstring="""
                            Gets/sets the number of motor steps made by the Birefringent Filter when
                            the manual control button is pressed for a short time.
                            """)
+
+    @staticmethod
+    def _int_parser(value: str) -> int:
+        return int(value.split()[-1])
+
+    @staticmethod
+    def _float_parser(value: str) -> float:
+        return float(value.split()[-1])
 
     @staticmethod
     def _status_parser(status: int) -> dict:
@@ -118,8 +129,7 @@ class SirahMatisseMotor(InstrumentChannel, abc.ABC):
         Returns:
             Dictionary with following keys:
                 - raw: Same as argument status
-                - status: Current status of the controller (bits 0 to 6)
-                - is_error: Set in case of an error status of the controller (bit 7)
+                - status: Current status of the controller, negative means error (bits 0 to 7)
                 - motor_running: Indicates that the motor is running (bit 8)
                 - motor_current_off: Indicates the motor current is switched off (bit 9)
                 - invalid_motor_pos: Indicates an invalid motor position after hold was switched off
@@ -130,15 +140,14 @@ class SirahMatisseMotor(InstrumentChannel, abc.ABC):
                 - manual_control: Manual control enable/disable (bit 14)
         """
         return dict(raw=status,
-                    status=status & 0x7f,                        # bits 0 to 6
-                    is_error=bool((status >> 7) & 1),            # bit 7
-                    motor_running=bool((status >> 8) & 1),       # bit 8
-                    motor_current_off=bool((status >> 9) & 1),   # bit 9
+                    status=status & 0xff,  # bits 0 to 7
+                    motor_running=bool((status >> 8) & 1),  # bit 8
+                    motor_current_off=bool((status >> 9) & 1),  # bit 9
                     invalid_motor_pos=bool((status >> 10) & 1),  # bit 10
-                    limit_switch_1=bool((status >> 11) & 1),     # bit 11
-                    limit_switch_2=bool((status >> 12) & 1),     # bit 12
-                    home_switch=bool((status >> 13) & 1),        # bit 13
-                    manual_control=bool((status >> 14) & 1))     # bit 14
+                    limit_switch_1=bool((status >> 11) & 1),  # bit 11
+                    limit_switch_2=bool((status >> 12) & 1),  # bit 12
+                    home_switch=bool((status >> 13) & 1),  # bit 13
+                    manual_control=bool((status >> 14) & 1))  # bit 14
 
 
 class SirahMatisseBiFiMotor(SirahMatisseMotor):
@@ -148,7 +157,7 @@ class SirahMatisseBiFiMotor(SirahMatisseMotor):
         # Additional parameters for BiFi-motor
         self.add_function("move_constant_absolute",
                           call_cmd=self._cmd_prefix + "CABS {}",
-                          vals=(vals.Ints(),),
+                          args=(vals.Ints(),),
                           # TODO unit="???",
                           docstring="""
                           Move the stepper motor of the birefringent filter to an absolute position
@@ -159,7 +168,7 @@ class SirahMatisseBiFiMotor(SirahMatisseMotor):
 
         self.add_function("move_constant_relative",
                           call_cmd=self._cmd_prefix + "CREL {}",
-                          vals=(vals.Ints(),),
+                          args=(vals.Ints(),),
                           # TODO unit="???",
                           docstring="""
                           Move the stepper motor of the birefringent filter relative to its current
@@ -173,6 +182,7 @@ class SirahMatisseBiFiMotor(SirahMatisseMotor):
                            label="Step frequency of birefringent filter motor",
                            get_cmd=self._cmd_prefix + "FREQ?",
                            set_cmd=self._cmd_prefix + "FREQ {}",
+                           get_parser=self._int_parser,
                            vals=vals.Ints(),
                            # TODO unit="???", # Hz?
                            docstring="""
@@ -184,7 +194,8 @@ class SirahMatisseBiFiMotor(SirahMatisseMotor):
                            label="BiFi-position in terms of a wavelength",
                            get_cmd=self._cmd_prefix + "WL?",
                            set_cmd=self._cmd_prefix + "WL {}",
-                           vals=vals.Ints(),
+                           get_parser=self._float_parser,
+                           vals=vals.Numbers(),
                            unit="nm",
                            docstring="""
                            Gets/moves the current position of the birefringent filter in terms of a
@@ -198,6 +209,7 @@ class SirahMatisseBiFiMotor(SirahMatisseMotor):
                                  "filter motor position",
                            get_cmd=self._cmd_prefix + "CAVSCN?",
                            set_cmd=self._cmd_prefix + "CAVSCN {}",
+                           get_parser=self._float_parser,
                            vals=vals.Numbers(),
                            # TODO unit="???",
                            docstring="""
@@ -210,6 +222,7 @@ class SirahMatisseBiFiMotor(SirahMatisseMotor):
                                  "filter position",
                            get_cmd=self._cmd_prefix + "REFSCN?",
                            set_cmd=self._cmd_prefix + "REFSCN {}",
+                           get_parser=self._float_parser,
                            vals=vals.Numbers(),
                            # TODO unit="???",
                            docstring="""
@@ -223,6 +236,7 @@ class SirahMatisseBiFiMotor(SirahMatisseMotor):
                            get_cmd=self._cmd_prefix + "MOTOFF?",
                            set_cmd=self._cmd_prefix + "MOTOFF {}",
                            vals=vals.Numbers(),
+                           get_parser=self._float_parser,
                            # TODO unit="???",
                            docstring="""
                            Gets/sets the calibration parameter *WavelengthOffset* for the step motor
@@ -233,6 +247,7 @@ class SirahMatisseBiFiMotor(SirahMatisseMotor):
                            label="Calibration parameter for wavelength factor",
                            get_cmd=self._cmd_prefix + "MOTFAC?",
                            set_cmd=self._cmd_prefix + "MOTFAC {}",
+                           get_parser=self._float_parser,
                            vals=vals.Numbers(),
                            # TODO unit="???",
                            docstring="""
@@ -244,6 +259,7 @@ class SirahMatisseBiFiMotor(SirahMatisseMotor):
                            label="Calibration parameter for lever length",
                            get_cmd=self._cmd_prefix + "MOTTHNS?",
                            set_cmd=self._cmd_prefix + "MOTTHNS {}",
+                           get_parser=self._float_parser,
                            vals=vals.Numbers(),
                            # TODO unit="???",
                            docstring="""
@@ -255,6 +271,7 @@ class SirahMatisseBiFiMotor(SirahMatisseMotor):
                            label="Calibration parameter for linear offset",
                            get_cmd=self._cmd_prefix + "ORDER?",
                            set_cmd=self._cmd_prefix + "ORDER {}",
+                           get_parser=self._float_parser,
                            vals=vals.Numbers(),  # or maybe integer (manual is not clear on that)
                            # TODO unit="???",
                            docstring="""
@@ -270,7 +287,7 @@ class SirahMatisseThEtMotor(SirahMatisseMotor):
 
 class SirahMatisse(VisaInstrument):
     def __init__(self, name: str, address: str):
-        super().__init__(name, address, terminator="\r\n")
+        super().__init__(name, address)
 
         motor_channels = ChannelList(self, "motor_channels", SirahMatisseMotor, snapshotable=False)
 
@@ -285,4 +302,29 @@ class SirahMatisse(VisaInstrument):
         motor_channels.lock()
         self.add_submodule("motor_channels", motor_channels)
 
+        self.add_function("clear_errors",
+                          call_cmd="ERR:CL",
+                          docstring="")  # TODO
+
+        self.add_parameter("power_diode_dc",
+                           get_cmd="DPOW:DC?",
+                           get_parser=self._float_parser,
+                           vals=vals.Numbers(),
+                           # TODO unit="???",
+                           docstring="")  # TODO
+
+        self.add_parameter("te_reflex",
+                           get_cmd="TE:DC?",
+                           get_parser=self._float_parser,
+                           vals=vals.Numbers(),
+                           # TODO unit="???",
+                           docstring="")  # TODO
+
         self.connect_message()
+
+    def write(self, *args, **kwargs):
+        self.ask(*args, *kwargs)
+
+    @staticmethod
+    def _float_parser(value: str) -> float:
+        return float(value.split()[-1])
